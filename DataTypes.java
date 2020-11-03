1726e02d8d8f1f334d0f1afb185ab8e7691bb6bb
longnv41 <73681216+longnv41@users.noreply.github.com>
2020-11-03 21:04:43 +0700

    first commit

---
+++ DataTypes.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datatypes;
+
+/**
+ *
+ * @author sss
+ */
+public class DataTypes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("size of byte in bit: "+Byte.SIZE);
+        System.out.println("min value of byte: "+Byte.MIN_VALUE);   
+        System.out.println("max value of byte: "+Byte.MAX_VALUE);
+        
+        System.out.println("size of Short in bit: "+Short.SIZE);
+        System.out.println("min value of Short: "+Short.MIN_VALUE);   
+        System.out.println("max value of Short: "+Short.MAX_VALUE);
+        
+        System.out.println("size of Int in bit: "+Integer.SIZE);
+        System.out.println("min value of Int: "+Integer.MIN_VALUE);   
+        System.out.println("max value of Int: "+Integer.MAX_VALUE);
+        
+        System.out.println("size of long in bit: "+Long.SIZE);
+        System.out.println("min value of long: "+Long.MIN_VALUE);   
+        System.out.println("max value of long: "+Long.MAX_VALUE);
+        
+        System.out.println("size of Float in bit: "+Float.SIZE);
+        System.out.println("min value of Float: "+Float.MIN_VALUE);   
+        System.out.println("max value of Float: "+Float.MAX_VALUE);
+        
+        System.out.println("size of Double in bit: "+Double.SIZE);
+        System.out.println("min value of Double: "+Double.MIN_VALUE);   
+        System.out.println("max value of Double: "+Double.MAX_VALUE);
+        
+        System.out.println("true of false? " +(99>3));
+    }
+    
+}
---
+++ Variable.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datatypes;
+
+/**
+ *
+ * @author sss
+ */
+public class Variable {
+    public static void main(String[] args){
+        int a=0;
+        int b=125;//khoi tao cho bien
+        int c,d=-9,e=999,f;
+        float PI=3.141592f;//cuối của kiểu float phải có f hoa or thường.
+        char k='u';
+        boolean bool=true;//lưu trữ giá trị true hay false thì sd boolean
+        float mark=9.5f;
+        int sumOfAAndB = a+b;
+        float r=6.25f;
+        float area=PI*r*r;
+        System.out.println("Sum = "+sumOfAAndB);
+        System.out.println("circle area: "+area);
+        
+    }
+    
+    
+}
